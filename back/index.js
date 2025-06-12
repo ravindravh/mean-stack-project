687fab71411132e4fef41b1bb1fd0dad2ad8c8a0
Ravindra Hegde <hegderavindra0@gmail.com>
2025-06-12 17:06:41 +0530

    cors updated

--- back/index.js
+++ back/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 require("./db/config")
 const Users = require("./db/user")
+const cors = require('cors')
 const app =express()
+app.use(cors())
 app.use(express.json())
 app.post("/register",async (req,resp) => {
     let user= new Users(req.body)
