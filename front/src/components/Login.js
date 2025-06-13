8c91c7a0165dcbfa93a03f0c772d6408c7f3f31d
Ravindra Hegde <hegderavindra0@gmail.com>
2025-06-13 18:24:00 +0530

    login form ready

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate =useNavigate()
+  const handleLogin = () => {
+    console.warn(email,password)
+
+  }
   return (
-    <div>
-        <h1>Login</h1>
+    <div className='login'>
+        <input className='inputbox' type='text' onChange={(e)=> setEmail(e.target.value)}  value={email} placeholder='Enter Email' />
+        <input className='inputbox' type='password' onChange={(e)=> setPassword(e.target.value)} value={password} placeholder='Enter Password' />
+        <button className='appButton' type='button' onClick={handleLogin} > SignUp </button>
     </div>
   )
 }
