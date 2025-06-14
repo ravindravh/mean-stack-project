5f5e0781e3bb27f10845cc9e41205e00e2380024
Ravindra Hegde <hegderavindra0@gmail.com>
2025-06-14 16:16:25 +0530

    login logout completed

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -5,15 +5,36 @@ const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const navigate =useNavigate()
-  const handleLogin = () => {
+  useEffect(() => {
+    const auth = localStorage.getItem('user')
+    if(auth){
+      navigate('/')
+    }
+  },[])
+  const handleLogin = async () => {
     console.warn(email,password)
+    let result = await fetch("http://localhost:5000/login", {
+      method : 'post',
+      body : JSON.stringify({email, password}),
+      headers : {
+        'Content-type' : 'Application/json'
+      }
 
+    })
+    result = await result.json()
+    console.warn(result)
+    if(result.name){
+      localStorage.setItem("user",JSON.stringify(result))
+      navigate('/')      
+    }else{
+      alert("Please enter correct details")
+    }
   }
   return (
     <div className='login'>
         <input className='inputbox' type='text' onChange={(e)=> setEmail(e.target.value)}  value={email} placeholder='Enter Email' />
         <input className='inputbox' type='password' onChange={(e)=> setPassword(e.target.value)} value={password} placeholder='Enter Password' />
-        <button className='appButton' type='button' onClick={handleLogin} > SignUp </button>
+        <button className='appButton' type='button' onClick={handleLogin} > Login </button>
     </div>
   )
 }
--- front/src/components/Nav.js
+++ front/src/components/Nav.js
@@ -10,18 +10,19 @@ const Nav = ()=>{
     }
     return(
         <div>
-            <ul className='nav-ul'>
+            
+            {auth ? 
+            <ul className='nav-ul' >                
                 <li><Link to ='/'> Products </Link> </li>
                 <li><Link to ='/add'> Add Product </Link> </li>
                 <li><Link to ='/update'> Update Product </Link> </li>                
-                <li><Link to ='/profile'> Profile </Link> </li>
-                <li><Link to ='/login'> Login </Link> </li>
-                <li>{auth ? <Link to ='/signup' onClick={logout} > Logout </Link>  :<Link to ='/SignUp'> SignUp </Link> } </li>
-
-
-              
-                
+                <li><Link to ='/profile'> Profile </Link> </li>                
+                <li><Link to ='/login' onClick={logout} > Logout ({JSON.parse(auth).name}) </Link> </li> </ul> :
+                <ul className="nav-ul" >                
+                <li><Link to ='/login'> Login </Link></li>
+                <li><Link to ='/SignUp'> SignUp </Link></li>  
             </ul>
+            }
         </div>
     )
 }
